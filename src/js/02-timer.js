0151c81e769a332694b8685ac5ebc8614ea382eb
Ivan Kapustian <michiro2845@gmail.com>
2023-07-27 14:26:03 +0300

    final

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,30 +1,17 @@
-// Описаний в документації
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const inputData = document.querySelector('#datetime-picker');
-const startButton = document.querySelector('[data-start]');
-const btnStart = document.querySelector('[data-start]');
+const btnStart = document.querySelector('button[data-start]');
 const day = document.querySelector('[data-days]');
 const hour = document.querySelector('[data-hours]');
 const minute = document.querySelector('[data-minutes]');
 const second = document.querySelector('[data-seconds]');
+const inputData = document.querySelector('#datetime-picker');
+btnStart.disabled = true;
+let timerId = null;
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] < Date.now()) {
-      btnStart.disabled = true;
-      Notify.failure('Please choose a date in the future');
-      return;
-    }
-    btnStart.disabled = false;
-  },
-};
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -44,6 +31,20 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Please choose a date in the future');
+      return;
+    }
+    btnStart.disabled = false;
+  },
+};
+
 flatpickr(inputData, options);
 
 btnStart.addEventListener('click', startCount);
@@ -52,14 +53,18 @@ function startCount() {
   inputData.disabled = true;
   btnStart.disabled = true;
   timerId = setInterval(() => {
-    const selectedData = new Date(inputData.value);
-    const remainder = selectedData - Date.now();
-    const { days, hours, minutes, seconds } = convertMs(remainder);
-    day.textContent = addLeadingZero(days);
-    hour.textContent = addLeadingZero(hours);
-    minute.textContent = addLeadingZero(minutes);
-    second.textContent = addLeadingZero(seconds);
-    if (remainder < 1000) {
+    const selectedDates = new Date(inputData.value);
+    const remainingTime = convertMs(selectedDates - new Date());
+    day.textContent = addLeadingZero(remainingTime.days);
+    hour.textcontent = addLeadingZero(remainingTime.hours);
+    minute.textContent = addLeadingZero(remainingTime.minutes);
+    second.textContent = addLeadingZero(remainingTime.seconds);
+    if (
+      remainingTime.days === 0 &&
+      remainingTime.hours === 0 &&
+      remainingTime.minutes === 0 &&
+      remainingTime.seconds === 0
+    ) {
       Notify.success('The timer has expired');
       clearInterval(timerId);
       inputData.disabled = false;
